49bed19674031c05adcb4aaf805cf39da5845277
AlexKozel <mr.kozeljd@gmail.com>
2019-06-24 18:11:27 +0300

    almost ready nuulpoint

--- delivery_cost/src/Main.java
+++ delivery_cost/src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Count count = new Count();
+        count.getSum();
+        System.out.println(count.getSum());
     }
 }
