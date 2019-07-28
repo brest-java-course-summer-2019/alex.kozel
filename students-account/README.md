bfae085b45936e19c5f0565711c078c09aa5a413
AlexKozel <mr.kozeljd@gmail.com>
2019-07-28 13:21:49 +0300

    Modify_Readmy_and_Gitignore

--- students-account/README.md
+++ students-account/README.md
@@ -1,3 +1,5 @@
+    [![Build Status](https://travis-ci.org/brest-java-course-summer-2019/alex.kozel.svg?branch=master)](https://travis-ci.org/brest-java-course-summer-2019/alex.kozel)
+
     Students Account Application
 
     Project for accounting students on different courses, include two entiteles: Courses and Students, related as many to many
--- students-account/test-db/target/classes/create-tables.sql
+++ students-account/test-db/target/classes/create-tables.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS course;
 CREATE TABLE course (
   course_id INT NOT NULL AUTO_INCREMENT,
   course_name VARCHAR(255) NOT NULL UNIQUE,
+  course_date DATE NOT NULL,
   PRIMARY KEY (course_id)
 );
 
@@ -9,7 +10,6 @@ DROP TABLE IF EXISTS student;
 CREATE TABLE student (
   student_id INT NOT NULL AUTO_INCREMENT,
   student_name VARCHAR(255) NOT NULL,
-  date DATE NOT NULL,
   course_id INT NOT NULL,
   PRIMARY KEY (student_id),
   FOREIGN KEY (course_id) REFERENCES course(course_id)
