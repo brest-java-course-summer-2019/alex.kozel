5952ee83efbb2eec47d15bb4342c19e62ac5e3ce
AlexKozel <mr.kozeljd@gmail.com>
2019-07-29 13:20:45 +0300

    remove_target

--- students-account/test-db/target/classes/data-script.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO course (course_id, course_name, course_date) VALUES (1, 'DEV', date);
-INSERT INTO course (course_id, course_name, course_date) VALUES (2, 'ACCOUNTING', date);
-INSERT INTO course (course_id, course_name, course_date) VALUES (3, 'MANAGEMENT', date);
-
-INSERT INTO student (student_id, student_name, course_id) VALUES (1, 'FUSER10', 1);
-INSERT INTO student (student_id, student_name, course_id) VALUES (2, 'FUSER11', 1);
-INSERT INTO student (student_id, student_name, course_id) VALUES (3, 'FUSER12', 1);
-INSERT INTO student (student_id, student_name, course_id) VALUES (4, 'FUSER13', 2);
-INSERT INTO student (student_id, student_name, course_id) VALUES (5, 'FUSER14', 2);
