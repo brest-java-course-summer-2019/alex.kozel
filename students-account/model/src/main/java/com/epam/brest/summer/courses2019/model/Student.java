37ea8d89ea5cc74257ef804438d7e4bd56fac686
AlexKozel <mr.kozeljd@gmail.com>
2019-08-16 13:22:20 +0300

    Junit_Tests_Model

--- students-account/model/src/main/java/com/epam/brest/summer/courses2019/model/Student.java
+++ students-account/model/src/main/java/com/epam/brest/summer/courses2019/model/Student.java
@@ -4,8 +4,19 @@ package com.epam.brest.summer.courses2019.model;
  * POJO Student for model.
  */
 public class Student {
+    /**
+     * Student ID - Primary key
+     */
     private Integer studentId;
+
+    /**
+     * Student Name
+     */
     private String studentName;
+
+    /**
+     * Course Id - Foreigner key
+     */
     private Integer courseId;
 
 
@@ -17,26 +28,56 @@ public class Student {
         this.courseId = courseId;
     }
 
+    /**
+     *Returns <code>Integer</code> representation of this studentId.
+     *
+     *@return studentId Student Id.
+     */
     public Integer getStudentId() {
         return studentId;
     }
 
+    /**
+     * Sets the course's identifier.
+     *
+     * @param studentId Student Id.
+     */
     public void setStudentId(Integer studentId) {
         this.studentId = studentId;
     }
 
+    /**
+     * Returns <code>String</code> representation of this studentName.
+     *
+     * @return studentName Student Name.
+     */
     public String getStudentName() {
         return studentName;
     }
 
+    /**
+     * Sets the course's name.
+     *
+     * @param studentName Student Name.
+     */
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
 
+    /**
+     * Returns <code>Integer</code> representation of this courseId.
+     *
+     * @return courseId course Id.
+     */
     public Integer getCourseId() {
         return courseId;
     }
 
+    /**
+     * Sets the course's identifier.
+     *
+     * @param courseId Course Id.
+     */
     public void setCourseId(Integer courseId) {
         this.courseId = courseId;
     }
