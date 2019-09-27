0e6b7cd816fbcf2595a705da9475283fd675c438
AlexKozel <mr.kozeljd@gmail.com>
2019-09-27 15:05:55 +0300

    RestApp

--- students-account/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/validators/CourseValidator.java
+++ students-account/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/validators/CourseValidator.java
@@ -1,11 +1,13 @@
 package com.epam.brest.summer.courses2019.web_app.validators;
 
 import com.epam.brest.summer.courses2019.model.Course;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+@Component
 public class CourseValidator implements Validator {
 
     public static final int COURSE_NAME_MAX_SIZE = 255;
--- students-account/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/validators/StudentValidator.java
+++ students-account/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/validators/StudentValidator.java
@@ -1,11 +1,13 @@
 package com.epam.brest.summer.courses2019.web_app.validators;
 
 import com.epam.brest.summer.courses2019.model.Student;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+@Component
 public class StudentValidator implements Validator {
 
     public static final int STUDENT_NAME_MAX_SIZE = 255;
